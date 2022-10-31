f9ddcea4d29bd4bc5bf2a37a84a83fc72386d209
Alexander Malchevsky <malickalex1975@gmail.com>
2022-10-31 22:50:40 +0300

    refactor: replace reducers and metaReducers to the new file reducers.reducer.ts

--- kanban-app/src/app/redux/index.ts
+++ kanban-app/src/app/redux/index.ts
@@ -1,20 +1,9 @@
-import {
-  ActionReducerMap,
-  MetaReducer,
-} from '@ngrx/store';
-import { environment } from 'src/environments/environment';
-import { StateConfirm } from './reducers/confirm.reducer';
-import { StateNotification } from './reducers/notification.reducer';
-import { StateUser } from './reducers/user.reducer';
+import { StateConfirm } from "./reducers/confirm.reducer";
+import { StateNotification } from "./reducers/notification.reducer";
+import { StateUser } from "./reducers/user.reducer";
 
 export interface State {
   notification?: StateNotification;
   confirmation?: StateConfirm;
   userState?: StateUser;
 }
-
-export const reducers: ActionReducerMap<State> = {};
-
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
-  : [];
