7cebc6cd12c3753b6b08ddebae1f7e3b2da8cd48
Alex Kochnev <madjok87@icloud.com>
2022-11-01 22:56:06 +0300

    feat:fix bugs

--- kanban-app/src/app/app-routing.module.ts
+++ kanban-app/src/app/app-routing.module.ts
@@ -10,7 +10,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [
+    RouterModule.forRoot(
+      routes
+      // { enableTracing: true }
+    ),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- kanban-app/src/app/pages/welcome/welcome-routing.module.ts
+++ kanban-app/src/app/pages/welcome/welcome-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path: '',
     component: WelcomeComponent,
     children: [
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      // { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'registration', component: RegistrationComponent },
     ],
