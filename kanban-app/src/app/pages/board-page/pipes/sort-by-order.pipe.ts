558f854287cd6ad40a917a756d2056c49ca35208
IvanMakarishchev <denis.flank@gmail.com>
2022-11-11 20:44:05 +0200

    feat: sorting tasks

---
+++ kanban-app/src/app/pages/board-page/pipes/sort-by-order.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sortBy',
+})
+export class SortByPipe implements PipeTransform {
+  transform(array: any, field: string): any[] {
+    let sortedArray = [...array];
+    sortedArray.sort((a: any, b: any) => {
+      if (a[field] < b[field]) {
+        return -1;
+      } else if (a[field] > b[field]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return sortedArray;
+  }
+}
