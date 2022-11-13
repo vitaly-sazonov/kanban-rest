a762d2e21c5ada1955bc9b104ef365a43179e1ca
Alexander Malchevsky <malickalex1975@gmail.com>
2022-11-13 18:16:53 +0300

    feat: add BY localization

---
+++ kanban-app/src/app/developers-by.ts
@@ -0,0 +1,54 @@
+import { DEVELOPER } from './developers';
+
+export const DEVELOPERS_BY: DEVELOPER[] = [
+  {
+    id: 2,
+    firstName: 'Аляксандр',
+    secondName: 'Мальчэускі',
+    photoURL: 'https://avatars.githubusercontent.com/u/91604689?v=4',
+    git: 'https://github.com/malickalex1975',
+    features: [],
+    alt: '',
+    aboutAuthor:
+      'Прывітанне! Трохі пра сябе, у мяне тры страсці: вандраванні, аўтамабілі і праграмаванне. Трэцяя наймацнейшая. Я люблю кадзіць, і не важна які гэта праект, фінальны таск па Ангуляры ці нейкі радавы, не важна якая гэта мова, Java Script ці Python, я проста люблю гэта і аддаю гэтаму ўсяго сябе.',
+  },
+  {
+    id: 3,
+    firstName: 'Ivan',
+    secondName: 'Makarishchev',
+    photoURL: 'https://ivanmakarishchev.github.io/rsschool-cv/images/photo.jpg',
+    git: 'https://github.com/IvanMakarishchev',
+    features: ['Design'],
+    alt: '',
+    aboutAuthor:
+      "Hello world! My name is Ivan Makarishchev and I'm studying on Stage#2 JavaScript Front-End Course of Rolling Scopes school. This is our course project. I hope you'll like it!",
+  },
+  {
+    id: 0,
+    firstName: 'Yauheni',
+    secondName: 'Tsurankou',
+    photoURL: 'https://avatars.githubusercontent.com/u/41520794?v=4',
+    git: 'https://github.com/teleuzi',
+    features: ['Mentoring'],
+    alt: '',
+    aboutAuthor:
+      "Hi! My name is Yauheni and I'm mentor of this brilliant hardworking team.",
+  },
+  {
+    id: 1,
+    firstName: 'Александр',
+    secondName: 'Кочнев',
+    photoURL: 'https://avatars.githubusercontent.com/u/92919510?v=4',
+    git: 'https://github.com/alexkochnev1987',
+    features: [
+      'Тим лид',
+      'Авторизация',
+      'Регистрация',
+      'Страница приветствия',
+      'Структура проекта',
+    ],
+    alt: '',
+    aboutAuthor:
+      'Привет всем!Меня зовут Александр и это финальный курсовой проект по технологии Ангуляр. Несмотря на то,что есть много менеджеров задач я надеюсь что нах вам понравится больше других ведь мы сделали его с душой.',
+  },
+];
