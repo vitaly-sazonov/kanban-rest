b97e928f93453468861ebdfb14f0da05feff296f
Alexander Malchevsky <malickalex1975@gmail.com>
2022-11-16 23:36:06 +0300

    feat: merge develop

--- kanban-app/src/app/pages/main/main.component.ts
+++ kanban-app/src/app/pages/main/main.component.ts
@@ -38,7 +38,4 @@ export class MainComponent implements OnInit {
   rememberSearch() {
     this.storage.setItem(LAST_SEARCH, this.searchRequest);
   }
-  rememberSearch() {
-    this.storage.setItem(LAST_SEARCH, this.searchRequest);
-  }
 }
